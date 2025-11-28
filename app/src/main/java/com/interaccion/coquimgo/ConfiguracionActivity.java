a2f100bfaf84c11b72ea1c68c795e8ebfbfb1867
yungpuxiito <beckhamespejo@gmail.com>
2025-11-27 21:22:33 -0300

    firebase terminado

--- app/src/main/java/com/interaccion/coquimgo/ConfiguracionActivity.java
+++ app/src/main/java/com/interaccion/coquimgo/ConfiguracionActivity.java
@@ -48,6 +48,8 @@ public class ConfiguracionActivity extends AppCompatActivity {
         btnSeleccionIdioma = findViewById(R.id.btnSeleccionIdioma);
         btnVolverInicio = findViewById(R.id.btnVolverInicio);
 
+        //mati qliao le gusta la tula
+
         txtTitulo = findViewById(R.id.txtTitulo);
         cardModoOscuro = findViewById(R.id.cardModoOscuro);
         cardIdioma = findViewById(R.id.cardIdioma);
